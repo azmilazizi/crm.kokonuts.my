857b111a0171bb1c477ef79b4e9f404463a07b4a
azmilazizi <azizihart99@gmail.com>
2025-03-10 00:32:51 +0800

    copied all files from cpanel

---
+++ application/logs/log-2025-03-08.php
@@ -0,0 +1,93 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-03-08 20:03:20 --> Severity: error --> Exception: Table 'kokonuts_crm.sessions' doesn't exist /home/kokonuts/public_html/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-08 20:14:49 --> Severity: error --> Exception: Table 'kokonuts_crm.sessions' doesn't exist /home/kokonuts/public_html/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-08 20:17:20 --> Severity: error --> Exception: Table 'kokonuts_crm.sessions' doesn't exist /home/kokonuts/public_html/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-08 20:29:37 --> Severity: error --> Exception: Table 'kokonuts_crm.sessions' doesn't exist /home/kokonuts/public_html/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-08 20:32:00 --> 404 Page Not Found: /index
+ERROR - 2025-03-08 20:32:07 --> 404 Page Not Found: /index
+ERROR - 2025-03-08 20:33:14 --> 404 Page Not Found: /index
+ERROR - 2025-03-08 20:33:18 --> Severity: error --> Exception: Table 'kokonuts_crm.sessions' doesn't exist /home/kokonuts/public_html/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-08 23:05:46 --> Could not find the language line "data_invalid"
+ERROR - 2025-03-08 23:05:46 --> Could not find the language line "data_invalid"
+ERROR - 2025-03-08 23:06:02 --> Could not find the language line "reset_data"
+ERROR - 2025-03-08 23:06:02 --> Could not find the language line "data_must_number"
+ERROR - 2025-03-08 23:12:26 --> Severity: 8192 --> Creation of dynamic property WarehouseLic::$check_interval_file is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/warehouse/libraries/gtsslib.php 59
+ERROR - 2025-03-08 23:15:00 --> Severity: 8192 --> Creation of dynamic property WarehouseLic::$check_interval_file is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/warehouse/libraries/gtsslib.php 59
+ERROR - 2025-03-08 23:19:15 --> Could not find the language line "Commodity List"
+ERROR - 2025-03-08 23:19:15 --> Could not find the language line "data_must_number"
+ERROR - 2025-03-08 23:19:39 --> Could not find the language line "Stock received manage"
+ERROR - 2025-03-08 23:19:39 --> Could not find the language line "send_received_note"
+ERROR - 2025-03-08 23:19:39 --> Could not find the language line "vendor"
+ERROR - 2025-03-08 23:19:39 --> Severity: Warning --> Undefined variable $vendors /home/kokonuts/public_html/crm.kokonuts.my/modules/warehouse/views/manage_goods_receipt/manage_purchase.php 84
+ERROR - 2025-03-08 23:19:39 --> Severity: Warning --> foreach() argument must be of type array|object, null given /home/kokonuts/public_html/crm.kokonuts.my/modules/warehouse/views/manage_goods_receipt/manage_purchase.php 84
+ERROR - 2025-03-08 23:19:39 --> Could not find the language line "subject"
+ERROR - 2025-03-08 23:19:39 --> Could not find the language line "attachment"
+ERROR - 2025-03-08 23:19:42 --> Could not find the language line "Inventory delivery voucher manage"
+ERROR - 2025-03-08 23:19:42 --> Could not find the language line "to"
+ERROR - 2025-03-08 23:19:42 --> Could not find the language line "address"
+ERROR - 2025-03-08 23:19:42 --> Could not find the language line "email"
+ERROR - 2025-03-08 23:19:44 --> Could not find the language line "Internal delivery note"
+ERROR - 2025-03-08 23:19:46 --> Could not find the language line "Commodity List"
+ERROR - 2025-03-08 23:19:46 --> Could not find the language line "data_must_number"
+ERROR - 2025-03-08 23:19:52 --> Could not find the language line "als_report"
+ERROR - 2025-03-08 23:19:52 --> Could not find the language line "total"
+ERROR - 2025-03-08 23:19:54 --> Could not find the language line "Commodity List"
+ERROR - 2025-03-08 23:19:54 --> Could not find the language line "data_must_number"
+ERROR - 2025-03-08 23:20:01 --> Could not find the language line "Commodity List"
+ERROR - 2025-03-08 23:20:01 --> Could not find the language line "data_must_number"
+ERROR - 2025-03-08 23:21:39 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:21:39 --> Could not find the language line "webcame"
+ERROR - 2025-03-08 23:21:39 --> Could not find the language line "barcode"
+ERROR - 2025-03-08 23:21:39 --> Could not find the language line "Payment"
+ERROR - 2025-03-08 23:21:39 --> Could not find the language line "Choose a payment method"
+ERROR - 2025-03-08 22:21:39 --> 404 Page Not Found: /index
+ERROR - 2025-03-08 23:21:40 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:21:49 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:21:49 --> Could not find the language line "processing"
+ERROR - 2025-03-08 23:21:50 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:22:21 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:22:21 --> Could not find the language line "active"
+ERROR - 2025-03-08 23:22:21 --> Could not find the language line "active"
+ERROR - 2025-03-08 23:22:21 --> Could not find the language line "active"
+ERROR - 2025-03-08 23:22:25 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:22:28 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:22:30 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:22:31 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:22:31 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:22:33 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:22:33 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:22:33 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:23:10 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:23:11 --> Could not find the language line "note_setting_2"
+ERROR - 2025-03-08 23:23:11 --> Severity: 8192 --> Creation of dynamic property Asynclibrary::$ci is deprecated /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/libraries/Asynclibrary.php 8
+ERROR - 2025-03-08 23:44:21 --> Could not find the language line "note_setting_2"
+ERROR - 2025-03-08 23:52:28 --> Could not find the language line "note_setting_2"
+ERROR - 2025-03-08 23:53:12 --> Severity: 8192 --> Optional parameter $warehouse_id declared before required parameter $invoice_id is implicitly treated as a required parameter /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/models/Omni_sales_model.php 2321
+ERROR - 2025-03-08 23:53:12 --> Could not find the language line "note_setting_2"
+ERROR - 2025-03-08 23:53:13 --> Severity: 8192 --> Optional parameter $warehouse_id declared before required parameter $invoice_id is implicitly treated as a required parameter /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/models/Omni_sales_model.php 2321
+ERROR - 2025-03-08 23:54:01 --> Severity: error --> Exception: syntax error, unexpected variable "$warehouse_id", expecting ")" /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/models/Omni_sales_model.php 2321
+ERROR - 2025-03-08 23:54:50 --> Could not find the language line "note_setting_2"
+ERROR - 2025-03-08 23:54:53 --> Could not find the language line "processing"
+ERROR - 2025-03-08 23:54:54 --> Could not find the language line "active"
+ERROR - 2025-03-08 23:54:54 --> Could not find the language line "active"
+ERROR - 2025-03-08 23:54:54 --> Could not find the language line "active"
+ERROR - 2025-03-08 23:54:58 --> Could not find the language line "chanel"
+ERROR - 2025-03-08 23:54:58 --> Could not find the language line "chanel"
+ERROR - 2025-03-08 23:54:58 --> Could not find the language line "chanel"
+ERROR - 2025-03-08 23:54:58 --> Could not find the language line "company"
+ERROR - 2025-03-08 23:55:05 --> Severity: Warning --> Undefined variable $payments_years /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/views/report/manage.php 57
+ERROR - 2025-03-08 23:55:05 --> Severity: Warning --> foreach() argument must be of type array|object, null given /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/views/report/manage.php 57
+ERROR - 2025-03-08 23:55:08 --> Could not find the language line "webcame"
+ERROR - 2025-03-08 23:55:08 --> Could not find the language line "barcode"
+ERROR - 2025-03-08 23:55:08 --> Could not find the language line "Payment"
+ERROR - 2025-03-08 23:55:08 --> Could not find the language line "Choose a payment method"
+ERROR - 2025-03-08 22:55:08 --> 404 Page Not Found: /index
+ERROR - 2025-03-08 23:55:17 --> Severity: Warning --> Undefined variable $payments_years /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/views/report/manage.php 57
+ERROR - 2025-03-08 23:55:17 --> Severity: Warning --> foreach() argument must be of type array|object, null given /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/views/report/manage.php 57
+ERROR - 2025-03-08 23:55:33 --> Could not find the language line "sales"
+ERROR - 2025-03-08 23:55:33 --> Could not find the language line "category"
+ERROR - 2025-03-08 23:55:41 --> Severity: Warning --> Undefined variable $payments_years /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/views/report/manage.php 57
+ERROR - 2025-03-08 23:55:41 --> Severity: Warning --> foreach() argument must be of type array|object, null given /home/kokonuts/public_html/crm.kokonuts.my/modules/omni_sales/views/report/manage.php 57
+ERROR - 2025-03-08 23:55:44 --> Could not find the language line "note_setting_2"
+ERROR - 2025-03-08 23:55:47 --> Could not find the language line "processing"
---
+++ application/logs/log-2025-03-09.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-03-09 04:06:56 --> Severity: error --> Exception: Table 'kokonuts_crm.staff' doesn't exist /home/kokonuts/public_html/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
