a497eea138de44095a61dfb386223b7997742535
azmilazizi <azizihart99@gmail.com>
2025-03-14 02:08:10 +0800

    added cors options in index

--- index.php
+++ index.php
@@ -94,6 +94,13 @@ switch (ENVIRONMENT)
 		exit(1); // EXIT_ERROR
 }
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    exit(0);
+}
+
 /*
  *---------------------------------------------------------------
  * SYSTEM DIRECTORY NAME
