5419a7270abcc934080acb2598ad8d957cb9de84
azmilazizi <azizihart99@gmail.com>
2025-03-14 03:43:40 +0800

    added new cors code

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ switch (ENVIRONMENT)
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, authtoken");
     http_response_code(200); // ✅ Always return HTTP 200 OK for OPTIONS
     exit();
 }
