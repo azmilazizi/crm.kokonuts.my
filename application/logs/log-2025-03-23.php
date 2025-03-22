5f707b1dd05a00ca569c152d88f80e0b83ae77f1
azmilazizi <azizihart99@gmail.com>
2025-03-23 02:16:38 +0800

    fixed hrm module

---
+++ application/logs/log-2025-03-23.php
@@ -0,0 +1,118 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-03-23 00:06:16 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:02 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:04 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:04 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 589
+ERROR - 2025-03-23 00:28:04 --> Could not find the language line ""
+ERROR - 2025-03-23 00:28:04 --> Severity: 8192 --> mb_strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 611
+ERROR - 2025-03-23 00:28:04 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 589
+ERROR - 2025-03-23 00:28:04 --> Could not find the language line ""
+ERROR - 2025-03-23 00:28:04 --> Severity: 8192 --> mb_strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 611
+ERROR - 2025-03-23 00:28:04 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 589
+ERROR - 2025-03-23 00:28:04 --> Could not find the language line ""
+ERROR - 2025-03-23 00:28:04 --> Severity: 8192 --> mb_strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 611
+ERROR - 2025-03-23 00:28:04 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 589
+ERROR - 2025-03-23 00:28:04 --> Could not find the language line ""
+ERROR - 2025-03-23 00:28:04 --> Severity: 8192 --> mb_strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 611
+ERROR - 2025-03-23 00:28:11 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:13 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:14 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:32 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:32 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:33 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:35 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:37 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:40 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:43 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:43 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 589
+ERROR - 2025-03-23 00:28:43 --> Could not find the language line ""
+ERROR - 2025-03-23 00:28:43 --> Severity: 8192 --> mb_strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 611
+ERROR - 2025-03-23 00:28:53 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:53 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:56 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:57 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:28:57 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 589
+ERROR - 2025-03-23 00:28:57 --> Could not find the language line ""
+ERROR - 2025-03-23 00:28:57 --> Severity: 8192 --> mb_strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 611
+ERROR - 2025-03-23 00:28:57 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 589
+ERROR - 2025-03-23 00:28:57 --> Could not find the language line ""
+ERROR - 2025-03-23 00:28:57 --> Severity: 8192 --> mb_strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 611
+ERROR - 2025-03-23 00:29:11 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:29:12 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:29:16 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:29:16 --> Could not find the language line "edit_requisition_m"
+ERROR - 2025-03-23 00:29:16 --> Could not find the language line "Requisition_information_already_exists"
+ERROR - 2025-03-23 00:29:17 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:29:17 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:29:18 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:30:34 --> Could not find the language line "create"
+ERROR - 2025-03-23 00:30:34 --> Severity: 8192 --> Creation of dynamic property HRPayrollLic::$check_interval_file is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hr_payroll/libraries/gtsslib.php 59
+ERROR - 2025-03-23 01:21:56 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:21:56 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 589
+ERROR - 2025-03-23 01:21:56 --> Could not find the language line ""
+ERROR - 2025-03-23 01:21:56 --> Severity: 8192 --> mb_strpos(): Passing null to parameter #1 ($haystack) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/application/helpers/general_helper.php 611
+ERROR - 2025-03-23 01:39:54 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:39:54 --> Could not find the language line ""
+ERROR - 2025-03-23 01:40:22 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:40:23 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:40:23 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1 /Users/azmilazizi/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-23 01:56:31 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:56:31 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1 /Users/azmilazizi/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-23 01:56:35 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:56:37 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:56:37 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1 /Users/azmilazizi/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-23 01:56:59 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:58:13 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:58:13 --> Severity: error --> Exception: Unknown column 'test' in 'where clause' /Users/azmilazizi/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-23 01:58:32 --> Could not find the language line "create"
+ERROR - 2025-03-23 01:58:32 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1 /Users/azmilazizi/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-23 01:59:58 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:00:01 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:00:03 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:00:04 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:00:06 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:00:07 --> Could not find the language line "features"
+ERROR - 2025-03-23 02:00:08 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:00:27 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:00:28 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:01:00 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:01:01 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:01:01 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:01:33 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:01:36 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:01:36 --> Could not find the language line ""
+ERROR - 2025-03-23 02:01:36 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1 /Users/azmilazizi/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-23 02:01:38 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:01:38 --> Could not find the language line ""
+ERROR - 2025-03-23 02:01:38 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1 /Users/azmilazizi/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-23 02:02:22 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:02:22 --> Could not find the language line ""
+ERROR - 2025-03-23 02:02:22 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1 /Users/azmilazizi/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-23 02:02:32 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:02:35 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:02:36 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:02:40 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:02:41 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:02:43 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:02:43 --> Could not find the language line ""
+ERROR - 2025-03-23 02:02:43 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1 /Users/azmilazizi/crm.kokonuts.my/system/database/drivers/mysqli/mysqli_driver.php 307
+ERROR - 2025-03-23 02:02:54 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:02:54 --> Could not find the language line ""
+ERROR - 2025-03-23 02:02:54 --> Severity: 8192 --> htmlspecialchars(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hrm/views/timekeeping/manage_dayoff.php 53
+ERROR - 2025-03-23 02:05:21 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:05:22 --> Could not find the language line ""
+ERROR - 2025-03-23 02:05:22 --> Severity: 8192 --> htmlspecialchars(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hrm/views/timekeeping/manage_dayoff.php 53
+ERROR - 2025-03-23 02:08:16 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:08:16 --> Could not find the language line ""
+ERROR - 2025-03-23 02:08:17 --> Severity: 8192 --> htmlspecialchars(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hrm/views/timekeeping/manage_dayoff.php 53
+ERROR - 2025-03-23 02:08:30 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:08:30 --> Could not find the language line ""
+ERROR - 2025-03-23 02:08:30 --> Severity: 8192 --> htmlspecialchars(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hrm/views/timekeeping/manage_dayoff.php 53
+ERROR - 2025-03-23 02:10:12 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:10:12 --> Could not find the language line ""
+ERROR - 2025-03-23 02:10:12 --> Severity: 8192 --> htmlspecialchars(): Passing null to parameter #1 ($string) of type string is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hrm/views/timekeeping/manage_dayoff.php 53
+ERROR - 2025-03-23 02:10:28 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:10:28 --> Could not find the language line ""
+ERROR - 2025-03-23 02:10:37 --> Could not find the language line "create"
+ERROR - 2025-03-23 02:10:37 --> Could not find the language line "create"
