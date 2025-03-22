ccd2b220b125681df073f0c80313da7155a4104f
azmilazizi <azizihart99@gmail.com>
2025-03-22 14:22:11 +0800

    fixed error in hr payroll plugin

---
+++ application/logs/log-2025-03-22.php
@@ -0,0 +1,41 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-03-22 13:17:56 --> Could not find the language line "create"
+ERROR - 2025-03-22 13:17:56 --> Severity: 8192 --> Creation of dynamic property HRPayrollLic::$check_interval_file is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hr_payroll/libraries/gtsslib.php 59
+ERROR - 2025-03-22 13:17:56 --> Could not find the language line "general_info"
+ERROR - 2025-03-22 13:17:56 --> Could not find the language line "hr_close"
+ERROR - 2025-03-22 05:17:57 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:17:57 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:17:57 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:17:57 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:17:57 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 13:22:11 --> Could not find the language line "create"
+ERROR - 2025-03-22 13:22:11 --> Severity: 8192 --> Creation of dynamic property HRPayrollLic::$check_interval_file is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hr_payroll/libraries/gtsslib.php 59
+ERROR - 2025-03-22 13:22:11 --> Could not find the language line "general_info"
+ERROR - 2025-03-22 13:22:11 --> Could not find the language line "hr_close"
+ERROR - 2025-03-22 05:22:13 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:22:13 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:22:13 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:22:13 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:22:13 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 13:44:13 --> Could not find the language line "create"
+ERROR - 2025-03-22 13:44:13 --> Severity: 8192 --> Creation of dynamic property HRPayrollLic::$check_interval_file is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hr_payroll/libraries/gtsslib.php 59
+ERROR - 2025-03-22 13:44:13 --> Could not find the language line "general_info"
+ERROR - 2025-03-22 13:44:13 --> Could not find the language line "hr_close"
+ERROR - 2025-03-22 05:44:15 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:44:15 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:44:15 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 05:44:15 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 14:14:30 --> Could not find the language line "create"
+ERROR - 2025-03-22 14:14:30 --> Severity: 8192 --> Creation of dynamic property HRPayrollLic::$check_interval_file is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hr_payroll/libraries/gtsslib.php 59
+ERROR - 2025-03-22 14:14:30 --> Could not find the language line "general_info"
+ERROR - 2025-03-22 14:14:30 --> Could not find the language line "hr_close"
+ERROR - 2025-03-22 06:14:33 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 06:14:33 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 06:14:33 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 06:14:33 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 06:14:33 --> 404 Page Not Found: /index
+ERROR - 2025-03-22 14:21:36 --> Could not find the language line "create"
+ERROR - 2025-03-22 14:21:36 --> Severity: 8192 --> Creation of dynamic property HRPayrollLic::$check_interval_file is deprecated /Users/azmilazizi/crm.kokonuts.my/modules/hr_payroll/libraries/gtsslib.php 59
+ERROR - 2025-03-22 14:21:36 --> Could not find the language line "general_info"
+ERROR - 2025-03-22 14:21:36 --> Could not find the language line "hr_close"
