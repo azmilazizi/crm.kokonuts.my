9721d16be583229c199ba34e57e0b2ce6efee9fb
azmilazizi <azizihart99@gmail.com>
2025-04-01 01:43:22 +0800

    changed expense->inventory asset account in Accounting_model.php

--- modules/accounting/models/Accounting_model.php
+++ modules/accounting/models/Accounting_model.php
@@ -14483,7 +14483,7 @@ class Accounting_model extends App_Model
                     $data_insert[] = $node;
 
                     $node = [];
-                    $node['split'] = $item_automatic->expense_account;
+                    $node['split'] = $item_automatic->inventory_asset_account;
                     $node['account'] = $payment_account;
                     $node['item'] = $item_id;
                     $node['date'] = $purchase_order->order_date;
