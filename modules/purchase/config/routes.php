247a3467ab8924b1735f4159f347af49580a269d
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-11-11 18:25:37 +0800

    Add vendor retrieval endpoints to purchase API

---
+++ modules/purchase/config/routes.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$route['purchase/api/v1/(:any)'] = 'api_purchase/$1';
+$route['purchase/api/(:any)']    = 'api_purchase/$1';
