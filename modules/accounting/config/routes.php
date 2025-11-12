6e3e4b3f935951de18209df2126588be0197ac0c
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-11-12 19:48:37 +0800

    Harden accounting API against schema differences

--- modules/accounting/config/routes.php
+++ modules/accounting/config/routes.php
@@ -1,4 +1,5 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$route['accounting/api/(:any)'] = 'api_accounting/$1';
+$route['accounting/api/v1/(.+)'] = 'api_accounting/$1';
+$route['accounting/api/(.+)']    = 'api_accounting/$1';
---
+++ modules/purchase/config/routes.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$route['purchase/api/v1/(.+)']  = 'api_purchase/$1';
+$route['purchase/api/(.+)']     = 'api_purchase/$1';
+$route['purchases/api/v1/(.+)'] = 'api_purchase/$1';
+$route['purchases/api/(.+)']    = 'api_purchase/$1';
