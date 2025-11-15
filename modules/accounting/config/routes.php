078405c612969c0d0b47120d0c8ea13282c2fbcb
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-11-15 17:46:02 +0800

    Add singular purchase API routes

--- modules/accounting/config/routes.php
+++ modules/accounting/config/routes.php
@@ -3,6 +3,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 // REST API routes for the Accounting module.
 $route = [
+    'accounting/api'         => 'api_accounting/index',
+    'accounting/api/'        => 'api_accounting/index',
+    'accounting/api/v1'      => 'api_accounting/index',
+    'accounting/api/v1/'     => 'api_accounting/index',
     'accounting/api/v1/(.+)' => 'api_accounting/$1',
     'accounting/api/(.+)'    => 'api_accounting/$1',
 ];
