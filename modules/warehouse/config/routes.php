5a91d4cf43ab68f38c223aceb617301dd18f4bc1
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-11-16 20:32:26 +0800

    Add legacy aliases for warehouse API

--- modules/warehouse/config/routes.php
+++ modules/warehouse/config/routes.php
@@ -1,4 +1,5 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$route['warehouse/api/(:any)'] = 'api_warehouse/$1';
+$route['warehouse/api/v1/(:any)'] = 'warehouse/api_warehouse/$1';
+$route['warehouse/api/(:any)']    = 'warehouse/api_warehouse/$1';
