09a8d66448f478f5958e88650ec6933b922daa6b
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-11-23 18:33:33 +0800

    Fix bill payment nested route resolution

--- modules/accounting/config/routes.php
+++ modules/accounting/config/routes.php
@@ -3,11 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 // REST API routes for the Accounting module.
 $route = [
+    'accounting/api/v1/bill/(\d+)/payments'    => 'api_accounting/bill_payments_by_bill/id/$1',
     'accounting/api/v1/bills/(\d+)/attachment' => 'api_accounting/bill_attachment/$1',
-    'accounting/api'         => 'api_accounting/index',
-    'accounting/api/'        => 'api_accounting/index',
-    'accounting/api/v1'      => 'api_accounting/index',
-    'accounting/api/v1/'     => 'api_accounting/index',
-    'accounting/api/v1/(.+)' => 'api_accounting/$1',
-    'accounting/api/(.+)'    => 'api_accounting/$1',
+    'accounting/api'                            => 'api_accounting/index',
+    'accounting/api/'                           => 'api_accounting/index',
+    'accounting/api/v1'                         => 'api_accounting/index',
+    'accounting/api/v1/'                        => 'api_accounting/index',
+    'accounting/api/v1/(.+)'                    => 'api_accounting/$1',
+    'accounting/api/(.+)'                       => 'api_accounting/$1',
 ];
