3d2fcf846413f6d83f18a4651359f6b7ed6606d4
azmilazizi <azmilazizi1999@graduate.utm.my>
2025-12-12 03:29:31 +0800

    fix

--- modules/accounting/models/Accounting_model.php
+++ modules/accounting/models/Accounting_model.php
@@ -11506,6 +11506,37 @@ class Accounting_model extends App_Model
     }
 
     /**
+     * Get the next journal entry number from options, resetting to 1 if missing.
+     *
+     * @return int
+     */
+    public function get_next_journal_entry_number()
+    {
+        $nextNumber = get_option('next_je_number');
+
+        if (!is_numeric($nextNumber) || (int) $nextNumber < 1) {
+            $nextNumber = 1;
+            update_option('next_je_number', $nextNumber);
+        }
+
+        return (int) $nextNumber;
+    }
+
+    /**
+     * Increment and persist the next journal entry number.
+     *
+     * @return int
+     */
+    public function increment_next_journal_entry_number()
+    {
+        $nextNumber = $this->get_next_journal_entry_number() + 1;
+
+        update_option('next_je_number', $nextNumber);
+
+        return $nextNumber;
+    }
+
+    /**
      * Format journal entry number based on journal date and optional provided number
      *
      * @param string|null $journalDate
