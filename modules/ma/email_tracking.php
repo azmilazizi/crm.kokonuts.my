ead1312e7f13759c4681b4245ebe7d3645d56018
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-12-18 09:59:59 +0800

    Add CORS headers to PHP entrypoints

--- modules/ma/email_tracking.php
+++ modules/ma/email_tracking.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, authtoken');
+
 $basePath = dirname(dirname(__DIR__));
 
 require_once $basePath . '/bootstrap/environment.php';
