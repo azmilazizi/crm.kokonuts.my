2647fb0bdebe2b44f2b497b07aa40dcb251144c2
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-12-20 01:52:38 +0800

    Fix journal entry attachment formatting

--- modules/accounting/config/routes.php
+++ modules/accounting/config/routes.php
@@ -7,6 +7,7 @@ $route = [
     'accounting/api/v1/bill/(\d+)/payment/(\d+)' => 'api_accounting/bill_payment_for_bill/$1/$2',
     'accounting/api/v1/bill/(\d+)/payments'    => 'api_accounting/bill_payments_by_bill/$1',
     'accounting/api/v1/bill/(\d+)/attachment' => 'api_accounting/bill_attachment/$1',
+    'accounting/api/v1/journal_entry/(\d+)/attachments' => 'api_accounting/journal_entry_attachments/$1',
     'accounting/api'                            => 'api_accounting/index',
     'accounting/api/'                           => 'api_accounting/index',
     'accounting/api/v1'                         => 'api_accounting/index',
