4d0a97a2e7f150a4c0207533ac4e757e1662bf0c
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-12-23 04:19:56 +0800

    Add staff token match API endpoint

--- application/config/api_endpoints.php
+++ application/config/api_endpoints.php
@@ -381,6 +381,18 @@ return [
                     ],
                 ],
                 [
+                    'group_prefix' => 'staff',
+                    'controller'   => 'api_staff_tokens',
+                    'routes'       => [
+                        [
+                            'path'                => 'token/(:num)',
+                            'action'              => 'token_match/$1',
+                            'methods'             => ['GET'],
+                            'with_trailing_slash' => true,
+                        ],
+                    ],
+                ],
+                [
                     'group_prefix' => 'goals',
                     'controller'   => 'goals/api_goals',
                     'routes'       => [
