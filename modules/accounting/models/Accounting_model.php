c49963bc6d69d315dad4cc43b8d74fe178bbb64a
azmilazizi <azmilazizi1999@graduate.utm.my>
2025-12-26 04:54:38 +0800

    latest

--- modules/accounting/models/Accounting_model.php
+++ modules/accounting/models/Accounting_model.php
@@ -10164,40 +10164,6 @@ class Accounting_model extends App_Model
                     $node['currency_rate'] = $currency_rate;
                     $data_insert[] = $node;
                 }
-
-                if(count($data_insert) == 0){
-                    if(get_option('acc_payment_automatic_conversion') == 1){
-                        $node = [];
-                        $node['split'] = $payment_account;
-                        $node['account'] = $deposit_to;
-                        $node['customer'] = $invoice->clientid;
-                        $node['debit'] = $payment_total;
-                        $node['credit'] = 0;
-                        $node['date'] = $payment->date;
-                        $node['description'] = '';
-                        $node['rel_id'] = $payment_id;
-                        $node['rel_type'] = 'payment';
-                        $node['datecreated'] = date('Y-m-d H:i:s');
-                        $node['addedfrom'] = get_staff_user_id();
-                        $node['currency_rate'] = $currency_rate;
-                        $data_insert[] = $node;
-
-                        $node = [];
-                        $node['split'] = $deposit_to;
-                        $node['customer'] = $invoice->clientid;
-                        $node['account'] = $payment_account;
-                        $node['date'] = $payment->date;
-                        $node['debit'] = 0;
-                        $node['credit'] = $payment_total;
-                        $node['description'] = '';
-                        $node['rel_id'] = $payment_id;
-                        $node['rel_type'] = 'payment';
-                        $node['datecreated'] = date('Y-m-d H:i:s');
-                        $node['addedfrom'] = get_staff_user_id();
-                        $node['currency_rate'] = $currency_rate;
-                        $data_insert[] = $node;
-                    }
-                }
             }else{
                 if(get_option('acc_payment_automatic_conversion') == 1){
                     $node = [];
