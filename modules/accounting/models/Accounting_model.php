6eb7e802ec1489f1b93bf2e61e3d27e4e87b88d0
azmilazizi <azmilazizi1999@graduate.utm.my>
2025-12-26 05:05:54 +0800

    latest

--- modules/accounting/models/Accounting_model.php
+++ modules/accounting/models/Accounting_model.php
@@ -10128,74 +10128,47 @@ class Accounting_model extends App_Model
                 $payment_total = round($currency_rate * $payment->amount, 2);
             }
 
-            if(get_option('acc_active_payment_mode_mapping') == 1){
+            $data_insert = [];
+            $use_payment_mapping = false;
+            if (get_option('acc_active_payment_mode_mapping') == 1) {
                 $payment_mode_mapping = $this->get_payment_mode_mapping($payment->paymentmode);
-                $data_insert = [];
-                if($payment_mode_mapping){
-                    $node = [];
-                    $node['split'] = $payment_mode_mapping->payment_account;
-                    $node['account'] = $payment_mode_mapping->deposit_to;
-                    $node['date'] = $payment->date;
-                    $node['debit'] = $payment_total;
-                    $node['customer'] = $invoice->clientid;
-                    $node['credit'] = 0;
-                    $node['tax'] = 0;
-                    $node['description'] = '';
-                    $node['rel_id'] = $payment_id;
-                    $node['rel_type'] = 'payment';
-                    $node['datecreated'] = date('Y-m-d H:i:s');
-                    $node['addedfrom'] = get_staff_user_id();
-                    $node['currency_rate'] = $currency_rate;
-                    $data_insert[] = $node;
-
-                    $node = [];
-                    $node['split'] = $payment_mode_mapping->deposit_to;
-                    $node['customer'] = $invoice->clientid;
-                    $node['account'] = $payment_mode_mapping->payment_account;
-                    $node['date'] = $payment->date;
-                    $node['tax'] = 0;
-                    $node['debit'] = 0;
-                    $node['credit'] = $payment_total;
-                    $node['description'] = '';
-                    $node['rel_id'] = $payment_id;
-                    $node['rel_type'] = 'payment';
-                    $node['datecreated'] = date('Y-m-d H:i:s');
-                    $node['addedfrom'] = get_staff_user_id();
-                    $node['currency_rate'] = $currency_rate;
-                    $data_insert[] = $node;
+                if ($payment_mode_mapping) {
+                    $payment_account = $payment_mode_mapping->payment_account;
+                    $deposit_to = $payment_mode_mapping->deposit_to;
+                    $use_payment_mapping = true;
                 }
-            }else{
-                if(get_option('acc_payment_automatic_conversion') == 1){
-                    $node = [];
-                    $node['split'] = $payment_account;
-                    $node['account'] = $deposit_to;
-                    $node['customer'] = $invoice->clientid;
-                    $node['debit'] = $payment_total;
-                    $node['credit'] = 0;
-                    $node['date'] = $payment->date;
-                    $node['description'] = '';
-                    $node['rel_id'] = $payment_id;
-                    $node['rel_type'] = 'payment';
-                    $node['datecreated'] = date('Y-m-d H:i:s');
-                    $node['addedfrom'] = get_staff_user_id();
-                    $node['currency_rate'] = $currency_rate;
-                    $data_insert[] = $node;
+            }
 
-                    $node = [];
-                    $node['split'] = $deposit_to;
-                    $node['customer'] = $invoice->clientid;
-                    $node['account'] = $payment_account;
-                    $node['date'] = $payment->date;
-                    $node['debit'] = 0;
-                    $node['credit'] = $payment_total;
-                    $node['description'] = '';
-                    $node['rel_id'] = $payment_id;
-                    $node['rel_type'] = 'payment';
-                    $node['datecreated'] = date('Y-m-d H:i:s');
-                    $node['addedfrom'] = get_staff_user_id();
-                    $node['currency_rate'] = $currency_rate;
-                    $data_insert[] = $node;
-                }
+            if ($use_payment_mapping || get_option('acc_payment_automatic_conversion') == 1) {
+                $node = [];
+                $node['split'] = $payment_account;
+                $node['account'] = $deposit_to;
+                $node['customer'] = $invoice->clientid;
+                $node['debit'] = $payment_total;
+                $node['credit'] = 0;
+                $node['date'] = $payment->date;
+                $node['description'] = '';
+                $node['rel_id'] = $payment_id;
+                $node['rel_type'] = 'payment';
+                $node['datecreated'] = date('Y-m-d H:i:s');
+                $node['addedfrom'] = get_staff_user_id();
+                $node['currency_rate'] = $currency_rate;
+                $data_insert[] = $node;
+
+                $node = [];
+                $node['split'] = $deposit_to;
+                $node['customer'] = $invoice->clientid;
+                $node['account'] = $payment_account;
+                $node['date'] = $payment->date;
+                $node['debit'] = 0;
+                $node['credit'] = $payment_total;
+                $node['description'] = '';
+                $node['rel_id'] = $payment_id;
+                $node['rel_type'] = 'payment';
+                $node['datecreated'] = date('Y-m-d H:i:s');
+                $node['addedfrom'] = get_staff_user_id();
+                $node['currency_rate'] = $currency_rate;
+                $data_insert[] = $node;
             }
 
             if($data_insert != []){
