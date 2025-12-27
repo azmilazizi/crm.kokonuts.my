11ed25eb31d75bb9d75516c6752ffeab41a31519
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-12-27 16:10:44 +0800

    Route warehouse history via legacy warehouse api

--- application/config/api_endpoints.php
+++ application/config/api_endpoints.php
@@ -413,11 +413,6 @@ return [
                     'controller'   => 'warehouse/api_warehouse',
                     'routes'       => [
                         [
-                            'path'    => 'warehouse_history',
-                            'action'  => 'warehouse_history',
-                            'methods' => ['GET'],
-                        ],
-                        [
                             'path'    => 'warehouses',
                             'action'  => 'warehouses',
                             'methods' => ['GET'],
