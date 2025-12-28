9b32103bd1f87f03d37cb90d55c5215086cf6088
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2025-12-28 15:29:45 +0800

    Disable global JWT expiry for multi-device access

--- application/config/jwt.php
+++ application/config/jwt.php
@@ -34,4 +34,4 @@ $config['token_header'] = 'authtoken';
 | ( 1 Hour ) : 60 * 60     = 3600
 | ( 1 Minute ) : 60        = 60
 */
-$config['token_expire_time'] = 86400;
+$config['token_expire_time'] = 0;
