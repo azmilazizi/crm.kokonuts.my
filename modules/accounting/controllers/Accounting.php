7620d1fa6734152fed8503f69fcb262e057f25f2
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2026-01-03 23:27:10 +0800

    Add bill options to bank import transaction modal

--- modules/accounting/controllers/Accounting.php
+++ modules/accounting/controllers/Accounting.php
@@ -10294,11 +10294,18 @@ class Accounting extends AdminController
             ->order_by('order_date', 'desc')
             ->get()
             ->result_array();
+        $data['purchase_bills'] = $this->db->select('pur_invoices.id, pur_invoices.invoice_number, pur_invoices.total, pur_invoices.invoice_date, ' . get_sql_select_vendor_company())
+            ->from(db_prefix() . 'pur_invoices')
+            ->join(db_prefix() . 'pur_vendor', db_prefix() . 'pur_vendor.userid = ' . db_prefix() . 'pur_invoices.vendor', 'left')
+            ->order_by('invoice_date', 'desc')
+            ->get()
+            ->result_array();
         $data['expense_categories'] = $this->db->select('id, name')
             ->from(db_prefix() . 'expenses_categories')
             ->order_by('name', 'asc')
             ->get()
             ->result_array();
+        $data['accounting_accounts'] = $this->accounting_model->get_accounts('', [], false);
 
         $this->load->view('banking/import_banking', $data);
     }
