0cc4aac0ffabcc3bfaafae090a93d5cc740c8ddc
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2026-01-04 02:12:31 +0800

    Align bank transaction matching to credit/debit

--- modules/accounting/controllers/Accounting.php
+++ modules/accounting/controllers/Accounting.php
@@ -3759,6 +3759,7 @@ class Accounting extends AdminController
 
         $filename ='';
         if($this->input->post()){
+            $bank_account = (int) $this->input->post('bank_account');
             if (isset($_FILES['file_csv']['name']) && $_FILES['file_csv']['name'] != '') {
                 $this->delete_error_file_day_before(1, ACCOUTING_IMPORT_ITEM_ERROR);
 
@@ -10403,14 +10404,13 @@ class Accounting extends AdminController
                             $matched_rel_id = '';
                             $matched_history_id = 0;
                             $amount = $spent > 0 ? $spent : $received;
+                            $match_field = $spent > 0 ? 'credit' : 'debit';
                             if($amount > 0){
-                                $build_query = function () use ($date_format, $amount, $matched_history_ids) {
+                                $build_query = function () use ($date_format, $amount, $match_field, $matched_history_ids, $bank_account) {
                                     $query = $this->db->select('id, rel_type, rel_id, cleared, bank_reconcile, (SELECT COUNT(*) FROM '.db_prefix().'acc_matched_transactions WHERE account_history_id = '.db_prefix().'acc_account_history.id) as matched_count')
                                         ->where('date', $date_format)
-                                        ->group_start()
-                                        ->where('debit', $amount)
-                                        ->or_where('credit', $amount)
-                                        ->group_end();
+                                        ->where('account', $bank_account)
+                                        ->where($match_field, $amount);
 
                                     if (!empty($matched_history_ids)) {
                                         $query->where_not_in('id', $matched_history_ids);
@@ -10662,14 +10662,14 @@ class Accounting extends AdminController
                 $received_amount = (float) str_replace([',', 'RM', 'rm', ' '], '', $received);
 
                 $amount = $spent_amount > 0 ? $spent_amount : $received_amount;
-                if ($amount > 0) {
-                    $build_query = function () use ($date_format, $amount, $matched_history_ids) {
+                $match_field = $spent_amount > 0 ? 'credit' : 'debit';
+                $bank_account = (int) ($row['bank_account'] ?? 0);
+                if ($amount > 0 && $bank_account > 0) {
+                    $build_query = function () use ($date_format, $amount, $match_field, $matched_history_ids, $bank_account) {
                         $query = $this->db->select('id, rel_type, rel_id, cleared, bank_reconcile, (SELECT COUNT(*) FROM '.db_prefix().'acc_matched_transactions WHERE account_history_id = '.db_prefix().'acc_account_history.id) as matched_count')
                             ->where('date', $date_format)
-                            ->group_start()
-                            ->where('debit', $amount)
-                            ->or_where('credit', $amount)
-                            ->group_end();
+                            ->where('account', $bank_account)
+                            ->where($match_field, $amount);
 
                         if (!empty($matched_history_ids)) {
                             $query->where_not_in('id', $matched_history_ids);
