6a1e72ea367c50d64806c86f773f8ea4bd7ccf45
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2026-02-05 21:43:38 +0800

    Fix payment progress approval source

--- modules/purchase/helpers/purchase_helper.php
+++ modules/purchase/helpers/purchase_helper.php
@@ -1797,14 +1797,14 @@ function purorder_inv_left_to_pay($pur_order){
 
     $list_applied_debit = $CI->purchase_model->get_inv_debit_purchase_order($pur_order);
     $paid = 0;
-    foreach($list_payment as $payment){
-        if($payment['approval_status'] == 2){
+    if($po && isset($po->approve_status) && (int)$po->approve_status === 2){
+        foreach($list_payment as $payment){
             $paid += $payment['amount'];
         }
-    }
 
-    foreach($list_applied_debit as $debit){
-        $paid += $debit['amount'];
+        foreach($list_applied_debit as $debit){
+            $paid += $debit['amount'];
+        }
     }
 
     if($po){
